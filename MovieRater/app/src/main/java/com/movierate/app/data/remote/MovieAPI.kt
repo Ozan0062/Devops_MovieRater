efa07667b72a96dd66cf679ca95fc60fefce771e
Ozan Huseyin Sabir <112860325+Ozan0062@users.noreply.github.com>
2026-09-10 16:23:48 +0200

    Delete MovieRater directory

--- MovieRater/app/src/main/java/com/movierate/app/data/remote/MovieAPI.kt
+++
@@ -1,9 +0,0 @@
-package com.movierate.app.data.remote
-
-import com.movierate.app.data.model.Movie
-import retrofit2.http.GET
-
-interface MovieApi {
-    @GET("movies")
-    suspend fun getMovies(): MovieResponse
-}
--- MovieRater/app/src/main/java/com/movierate/app/data/remote/MovieResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.movierate.app.data.remote
-
-import com.movierate.app.data.model.Movie
-
-data class MovieResponse(
-    val content: List<Movie>
-)
